d732d735a41a57dff40c2ac1c26457febdb3c78c
PUNITHA SHUNMUGAM S <110711185+punithashunmugam4@users.noreply.github.com>
2026-08-04 23:12:49 +0530

    Delete env.js

--- env.js
+++
@@ -1,7 +0,0 @@
-
-const _env={
-  GITHUB_USER : "punithashunmugam4",
-  GITHUB_REPO : "bot-automation-trees",
-  BRANCH : "main",
-  BOT_TREE_TOKEN : "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"
-}
